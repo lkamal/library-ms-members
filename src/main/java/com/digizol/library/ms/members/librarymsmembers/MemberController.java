055fa4f187501e079e1bb6bb81766c94bcb083f7
Kamal Mettananda <kamal_mettananda@persistent.com>
2022-09-07 17:27:25 +0530

    Included actuator to support liveliness and readiness probes. Also added few log lines into the controller.

--- src/main/java/com/digizol/library/ms/members/librarymsmembers/MemberController.java
+++ src/main/java/com/digizol/library/ms/members/librarymsmembers/MemberController.java
@@ -1,5 +1,7 @@
 package com.digizol.library.ms.members.librarymsmembers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +12,13 @@ import java.util.List;
 @RequestMapping("/members")
 public class MemberController {
 
+    private Logger logger = LoggerFactory.getLogger(MemberController.class);
+
     @GetMapping ("")
     public List<Member> list() {
+        logger.debug("Start method list()");
+        logger.info("Returning members, size = 2");
+        logger.debug("End method list()");
         return List.of(
                 new Member(1, "Rashmitha"),
                 new Member(2, "Dewmi")
